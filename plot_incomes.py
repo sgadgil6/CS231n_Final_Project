805bf1f2e0ce6f105009870732b1f932a3e2ea18
Shreya Singh <shreyasingh@DNa1c8698.SUNet>
2020-06-07 14:37:48 -0700

    .

--- plot_incomes.py
+++ plot_incomes.py
@@ -5,8 +5,8 @@ import copy
 # correct_labels_top5 = np.load("./data/correct_labels_top5_vgg16.npy")
 # incorrect_labels_top5 = np.load("./data/incorrect_labels_top5_vgg16.npy")
 
-correct_labels_top5 = np.load("./data/correct_labels_top5_income_vgg.npy", allow_pickle=True)
-incorrect_labels_top5 = np.load("./data/incorrect_labels_top5_income_vgg.npy", allow_pickle=True)
+correct_labels_top5 = np.load("./data/correct_labels_top5_focal_vgg_7.npy", allow_pickle=True)
+incorrect_labels_top5 = np.load("./data/incorrect_labels_top5_focal_vgg_7.npy", allow_pickle=True)
 
 print(len(correct_labels_top5[:, 2]))
 print(len(incorrect_labels_top5[:, 2]))
@@ -32,16 +32,18 @@ for low, high in income_buckets:
         continue
     accuracies.append(num_correct / (num_correct + num_incorrect))
 
-WINDOW_SIZE = 20
+WINDOW_SIZE = 10
 ma_acc = moving_average(accuracies, WINDOW_SIZE)
 low_incomes = [i for i, j in plot_income]
 # plt.plot([i for i, j in plot_income], accuracies)
 
 # Plot moving average
-plt. ylim(0.7,0.8)
+plt.ylim(0.7,1.0)
+# plt.scatter(low_incomes[len(low_incomes)-len(ma_acc):], ma_acc)
 plt.plot(low_incomes[len(low_incomes)-len(ma_acc):], ma_acc)
+
 plt.title("Acc v/s Income")
 plt.xlabel("Income Level")
 plt.ylabel("Accuracy")
-plt.savefig(PLOT_DIR+'income_loss_vgg.png', format='png')
-# plt.show()
+plt.savefig(PLOT_DIR+'focal_loss_vgg_7.png', format='png')
+plt.show()
